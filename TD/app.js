61ce727fffbe13574334a80e74175d5d176be854
K22 <ulrichkageu@gmail.com>
2023-12-26 11:26:51 +0100

    handlebars erro get/

--- TD/app.js
+++ TD/app.js
@@ -1,16 +1,25 @@
 const path = require('path');
+const { engine } = require('express-handlebars') //division html
 const express = require('express');
-const app = express();
-const port = 3080;
 
-app.use(express.static(path.join(__dirname, "views")))
-app.use(express.static(path.join(__dirname, "public")))
+const app = express();
+const port = 3090;
+
+app.engine('handlebars' , engine()); //obligatoir handlebars
+app.set('view engine' , 'handlebars');//obligatoir handlebars
+app.set ('views' , path.join(__dirname,  "views")) ;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get(' /' , (req , res) => { 
+   res.render('home')
+})
 
 /*
 app.get('/', (req, res) => {
     res.send("<h1>CATECHDIGITAL 2000000</h1>");
 });*/
 
-app.listen(port,  _=>{
+app.listen(port,  ()=>{
     console.log (`Connexion etablie sur le PORT : ${port}`)
 })
